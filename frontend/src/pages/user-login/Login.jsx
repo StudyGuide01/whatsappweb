8e83d9e228b57eeec1ffbf33652bef5041c386e0
varshamuteweb <muteweb.varshasahu@gmail.com>
2026-05-19 18:52:06 +0530

    create phone number and email yup validation checker function

--- frontend/src/pages/user-login/Login.jsx
+++ frontend/src/pages/user-login/Login.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import useLoginStore from "@/store/useLoginStore";
+import countries from "@/utils/countrilies";
+import React, { useState } from "react";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+//schema to check validation of state using yup
+const loginValidationSchema = yup
+  .object()
+  .shape({
+    phoneNumber: yup
+      .string()
+      .nullable()
+      .notRequired()
+      .matches(/^\d+$/, "phone number must be digit")
+      .transform((value, originalValue) => {
+        originalValue.trim() === "" ? null : value;
+      }),
+    email: yup
+      .string()
+      .nullable()
+      .notRequired()
+      .email("pelase enter valid email")
+      .transform((value, originalValue) => {
+        originalValue.trim() === "" ? null : value;
+      }),
+  })
+  .test(
+    "at-least-one",
+    "Either email or phoneNumber is required",
+    function (value) {
+      return !!(value.phoneNumber || value.email);
+    },
+  );
 
 const login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  const { step, userPhoneData, setStep, setUserPhoneData, resetLoginState } =
+    useLoginStore();
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState(countries[0]);
+  const [opt, setOtp] = useState(["", "", "", "", "", ""]);
+  const [email, setEmail] = useState("");
 
-export default login
+  return <div>Login</div>;
+};
+
+export default login;
